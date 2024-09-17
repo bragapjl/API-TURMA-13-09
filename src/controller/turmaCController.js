31a4e2e554b43cb5cb1f64650e933210477619cc
ryankkzy <ryan37370308@gmail.com>
2024-09-17 11:23:54 -0300

    alterei os controller

--- src/controller/turmaCController.js
+++ src/controller/turmaCController.js
@@ -14,3 +14,70 @@ endpoints.get('/turmaC', async (req,resp) => {
         })
     }
 })
+
+
+endpoints.post('/turmaC/' , async (req,resp) => {
+    try {
+        let turma = req.body;
+
+        let id = await db.inserirTurma(turma);
+
+        resp.send({
+            novoId: id
+        })
+
+    } 
+    catch (err) {
+        resp.status(400).send({
+            erro: err.message
+        })      
+    }
+})
+
+endpoints.put('/turmaC/:id' , async (req,resp) =>{
+    try {
+        let id = req.params.id;
+        let registros = req.body;
+
+        let linhasAfetadas = await db.alterarTurma(id , registros)
+
+        if(linhasAfetadas >=1 ){
+            resp.send()
+        } 
+        else {
+            resp.status(404).send({
+                erro: 'nenhum registro encontrado!'
+            })
+        }
+    } 
+    catch (err) {
+        resp.status(400).send({
+            erro: err.massage
+        })
+    }
+})
+
+endpoints.delete('/turmaC/:id', async (req,resp) => {
+    try {
+        let id = req.params.id;
+        let linhasAfetadas = await db.removerTurma(id);
+
+        if(linhasAfetadas >= 1 ){
+            resp.send()
+        } 
+        else {
+            resp.status(404).send({
+                erro: 'nenhum registro encontrado!'
+            })
+        }
+
+    } 
+    catch (err) {
+        resp.status(400).send({
+            erro: err.massage
+        })        
+    }
+})
+
+
+export default endpoints;
